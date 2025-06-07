6e4f7c6844732f19e393a78a7bbc2475539dcd07
voho <vojtech.hordejcuk@gmail.com>
2025-06-07 20:57:47 +0200

    dummy update

--- src/App.tsx
+++ src/App.tsx
@@ -20,12 +20,12 @@ const formatDate = (date: Date): string => {
 const images: GalleryImage[] = [
   {
     src: 'f1.jpg',
-    description: 'dummy image',
+    description: 'dummy image 1',
     date: new Date('2024-05-15')
   },
   {
     src: 'f2.jpg',
-    description: 'dummy image',
+    description: 'dummy image 2',
     date: new Date('2024-05-20')
   },
 ];
