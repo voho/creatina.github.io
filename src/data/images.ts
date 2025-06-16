494ef8468d9cb799b6024104fa7f860690c58c7f
voho <vojtech.hordejcuk@gmail.com>
2025-06-16 18:08:58 +0200

    fix paths

--- src/data/images.ts
+++ src/data/images.ts
@@ -2,57 +2,57 @@ import { GalleryImage } from '../types/gallery';
 
 const images: GalleryImage[] = [
   {
-    src: '/images/IMG_6719.jpg',
+    src: 'IMG_6719.jpg',
     description: 'Murder Drones character',
     date: new Date('2025-06-16')
   },
   {
-    src: '/images/IMG_6720.jpg',
+    src: 'IMG_6720.jpg',
     description: 'Murder Drones character',
     date: new Date('2025-06-16')
   },
   {
-    src: '/images/IMG_6721.jpg',
+    src: 'IMG_6721.jpg',
     description: 'Murder Drones character',
     date: new Date('2025-06-16')
   },
   {
-    src: '/images/IMG_6833.jpg',
+    src: 'IMG_6833.jpg',
     description: 'Murder Drones character',
     date: new Date('2025-06-16')
   },
   {
-    src: '/images/IMG_7089.jpg',
+    src: 'IMG_7089.jpg',
     description: 'Murder Drones character',
     date: new Date('2025-06-16')
   },
   {
-    src: '/images/IMG_7264.jpg',
+    src: 'IMG_7264.jpg',
     description: 'Murder Drones character',
     date: new Date('2025-06-16')
   },
   {
-    src: '/images/IMG_8735.jpg',
+    src: 'IMG_8735.jpg',
     description: 'Dandys World character',
     date: new Date('2025-06-16')
   },
   {
-    src: '/images/IMG_8736.jpg',
+    src: 'IMG_8736.jpg',
     description: 'Dandys World character',
     date: new Date('2025-06-16')
   },
   {
-    src: '/images/IMG_8737.jpg',
+    src: 'IMG_8737.jpg',
     description: 'Dandys World character',
     date: new Date('2025-06-16')
   },
   {
-    src: '/images/IMG_8738.jpg',
+    src: 'IMG_8738.jpg',
     description: 'Dandys World character',
     date: new Date('2025-06-16')
   },
   {
-    src: '/images/IMG_8739.jpg',
+    src: 'IMG_8739.jpg',
     description: 'Dandys World character',
     date: new Date('2025-06-16')
   }
