c38998eead731a464f16d4432ce13e5626635bb6
voho <vojtech.hordejcuk@gmail.com>
2025-06-16 18:05:52 +0200

    remove crappy import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Gallery from './components/Gallery';
-import { GalleryImage } from './types/gallery';
 import images from './data/images';
 
 const App = () => {
